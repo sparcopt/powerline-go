fa13171e04cf702f4e6ac9d1bb7c829c7f4703dd
sparcopt <j.l.p.almeida42@gmail.com>
2020-05-22 10:57:31 +0100

    Verify if folder is a Git repo

--- segment-gitversion.go
+++ segment-gitversion.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	pwl "github.com/justjanne/powerline-go/powerline"
 	"os/exec"
+	"strings"
 )
 
 type GitVersion struct {
@@ -11,16 +12,24 @@ type GitVersion struct {
 }
 
 func segmentGitVersion(p *powerline) []pwl.Segment {
+	if len(p.ignoreRepos) > 0 {
+		out, err := runGitCommand("git", "rev-parse", "--show-toplevel")
+		if err != nil {
+			return []pwl.Segment{}
+		}
+		out = strings.TrimSpace(out)
+		if p.ignoreRepos[out] {
+			return []pwl.Segment{}
+		}
+	}
 
 	out, err := exec.Command("dotnet-gitversion").Output()
-	
 	if err != nil {
         return []pwl.Segment{}
     }
 
 	var gitVersionOutput GitVersion
 	err = json.Unmarshal([]byte(out), &gitVersionOutput)
-
 	if err != nil || gitVersionOutput.MajorMinorPatch == "" {
 		return []pwl.Segment{}
 	}
